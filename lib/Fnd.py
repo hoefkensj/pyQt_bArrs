cb9edd7191d161712332aeecd6d8c325f4a1028a
hoefkensj <hoefkens.j@gmail.com>
2022-10-23 03:11:47 +0200

    Update .gitignore, .name, and 13 more files...

---
+++ lib/Fnd.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import inspect
+from  PyQt5.QtWidgets import QHBoxLayout,QVBoxLayout,QGridLayout,QFormLayout
+from  PyQt5.QtWidgets import QWidget
+
+def Qt_tools():
+	def Layouts():
+
+		l				=		{
+			'H'   :	QHBoxLayout,
+			'V'   : QVBoxLayout,
+			'G'   :	QGridLayout,
+			'F'   :	QFormLayout,
+							}
+		return l
+	def SizePols():
+		from PyQt5.QtWidgets.QSizePolicy import QSizePolicy as QSP
+		p={
+				'P'   : QSP.Preferred,
+				'M'   : QSP.Maximum,
+				'm'   : QSP.Minimum,
+				'E'   :	QSP.Expanding,
+				'mE'  :	QSP.MinimumExpanding,
+				'F'   :	QSP.Fixed,
+			}
+		return p
+	f={}
+	f['l']=Layouts()
+	f['p']=SizePols()
+	return f
+
+def Wgt_Func(fn):
+	def Mtds(key):
+		def clj_Mtds(o):
+			f={}
+			for n in dir(o[key]):
+				m=getattr(o, n)
+				if callable(m) and '__' not in n:
+					f[n]=m
+			return f
+		return clj_Mtds
+	f=locals()
+
+	return f[fn]
+
+def Wgt(**k):
+		QT=Qt_tools()
+		Mtds=Wgt_Func('Mtds')['Wgt']
+		def Arg(*a, **k):
+			n			= k.get("n")
+			t			= k.get("t")
+			m			=	k.get("m")
+			l			=	k.get("l")
+			k=locals()
+			return k[a]
+		def props():
+			name 		= Arg('n')
+			layout 	=	QT['l'][Arg('t')].__name__
+			return locals()
+		def Mtd():
+			f=Mtds(w)
+			return f
+		def Layout():
+			Mtds=Wgt_Func('Mtds')['Lay']
+			def Mtd():
+				f=Mtds(l)
+				return f
+			def Init():
+				def init():
+					l['Mtd']['setObjectName'](f'lay_{Arg("n")}')
+					l['Mtd']['setContentsMargins'](*Arg('m')
+					l['Mtd']['setSpacing'](0)
+				init()
+				return init
+			l={}
+			if Arg('t') is not None:
+				l['Lay']=QT['l'][Arg('t')](w['Wgt'])
+				l['Mtd']=Mtd()
+				l['Init']=Init()
+			return l
+		def Init():
+			def init():
+				w['Mtd']['setObjectName'](f'wgt_{Arg("n")}')
+				w['Mtd']['setContentsMargins'](*Arg('m'))
+			init()
+			return init
+
+		w= {}
+		w['Wgt']			=	QWidget()
+		w['Arg']			=	Arg()
+		w['Prop']			= props()
+		w['Mtd']			=	Mtd()
+		w['Lay']			= Layout()
+		w['Init']			=	Init()
+		return w
